f2b8b3ec2098fd7e41a14714dd88a7dee9252af4
LifeAsDev <angelocraft551@gmail.com>
2024-11-20 14:40:19 -0400

    create block modal enter keydown and focus input name

--- src/components/createBlockModal/createBlockModal.tsx
+++ src/components/createBlockModal/createBlockModal.tsx
@@ -1,6 +1,6 @@
 import { ScheduleBlock } from "@/src/components/scheduleTable/scheduleTable";
 import styles from "./styles.module.css";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef } from "react";
 
 export default function CreateBlockModal({
   scheduleBlock,
@@ -15,6 +15,12 @@ export default function CreateBlockModal({
   back: () => void;
   mode?: string;
 }) {
+  const activityInputRef = useRef<HTMLInputElement>(null);
+
+  // Enfocar el input al cargar el componente
+  useEffect(() => {
+    activityInputRef.current?.focus();
+  }, []);
   return (
     <div className={styles.overlay}>
       <div className={styles.mainBox}>
@@ -22,6 +28,7 @@ export default function CreateBlockModal({
         <div className={styles.inputBox}>
           <label htmlFor="activity">Actividad</label>
           <input
+            ref={activityInputRef} // Asignamos la ref al input
             placeholder="Nombre de actividad"
             id="activity"
             type="text"
@@ -35,6 +42,11 @@ export default function CreateBlockModal({
                 return newPrev;
               })
             }
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                createBlock(); // Llama a tu función aquí
+              }
+            }}
           />
         </div>
         <div className={styles.inputBox}>
